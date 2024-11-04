7efb73ed2d3993a4edcc2ded1d1d88343370d141
JailtonJunior14 <jailtonjrbr@gmail.com>
2024-11-04 15:55:19 -0300

    Iniciado.

---
+++ Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,21 @@
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+    }
+}
