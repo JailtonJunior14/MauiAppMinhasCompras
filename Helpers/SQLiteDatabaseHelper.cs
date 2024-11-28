629b23ed5059ad00d545c389697a6f8d7a4194e4
JailtonJunior14 <jailtonjrbr@gmail.com>
2024-11-28 14:32:16 -0300

    Não ta funcionando tudo

--- Helpers/SQLiteDatabaseHelper.cs
+++ Helpers/SQLiteDatabaseHelper.cs
@@ -27,6 +27,24 @@ namespace MauiAppMinhasCompras.Helpers
         {
             string sql = "UPDATE Produto" +
                 "SET DEscricao=?, Quantidade=?, Preco=? WHERE id=?";
-        }   
+            return _conn.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%'";
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
     }
 }
